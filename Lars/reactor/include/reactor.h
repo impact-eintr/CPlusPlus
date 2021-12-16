ffd927e1de83f03a9d5e50481c5a4ba6cc21139a
impact-eintr <yixingwei4@gmail.com>
2021-12-16 16:25:53 +0800

    提供一个公共头文件

---
+++ Lars/reactor/include/reactor.h
@@ -0,0 +1,21 @@
+#ifndef REACTOR_H_
+#define REACTOR_H_
+
+#include "io_buf.h"
+#include "buf_pool.h"
+#include "reactor_buf.h"
+
+#include "net_connection.h"
+#include "tcp_conn.h"
+#include "tcp_server.h"
+#include "tcp_client.h"
+
+#include "message.h"
+#include "task_msg.h"
+#include "event_loop.h"
+#include "thread_pool.h"
+#include "thread_queue.h"
+
+#include "config_file.h"
+
+#endif // REACTOR_H_
